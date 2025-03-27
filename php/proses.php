10e070966a613584051a9ee113e8de68658f78f3
Brian772 <ratatito2@gmail.com>
2025-03-27 22:01:54 +0700

    Update proses.php

--- php/proses.php
+++ php/proses.php
@@ -1,1 +1,23 @@
-
+<?php
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $secret_key = '6LekygErAAAAAPt3Hy2plhRToQ7a7gTpOCYvSOlV';
+    $captcha = $_POST['g-recaptcha-response'];
+    
+    // Verifikasi reCAPTCHA
+    $response = file_get_contents(
+        "https://www.google.com/recaptcha/api/siteverify?secret=$secret_key&response=$captcha"
+    );
+    $response = json_decode($response);
+    
+    if(!$response->success) {
+        die('Verifikasi reCAPTCHA gagal!');
+    }
+    
+    // Lanjutkan proses form
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    // Proses pengiriman email...
+}
+?>
